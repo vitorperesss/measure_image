84cb011c4efbbc730da6ba29cfd0995607f80035
Vitor Peres <vitorperessss@gmail.com>
2020-03-17 15:50:47 -0300

    Commit

--- index.php
+++ index.php
@@ -4,15 +4,17 @@
 <meta charset="utf-8">
 <title>Image Meter</title>
 </head>
+
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <link rel="stylesheet" type="text/css" href="style/style.css">
 <script type="text/javascript" src="js/main.js"></script>
 
 <body>
+	<img id="imagem" src="img/f2.jpeg" style="display: none">
 	<div class="controles">
-		<label id="undo" data-tooltip="Voltar"><img src="icon/baseline_reply_white_24dp.png" class="voltar hover"/></label>
+		<label id="undo" data-tooltip="Voltar"><img src="icon/baseline_reply_white_24dp.png" class="voltar hover"></label>
 		<label data-tooltip="Upload" class="custom-file-upload">
-			<img src="icon/baseline_cloud_download_white_24dp.png" class="download hover"/>
+			<img src="icon/baseline_cloud_download_white_24dp.png" class="download hover">
 			<input type="file" id="imageLoader"/>
 		</label>
 	</div>
